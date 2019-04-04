ea439f56c34a9b1ad686305b76a6071b6f2a671f
vue-demo <48950806+liuke21@users.noreply.github.com>
2019-04-04 21:44:45 +0800

    Update member.vue

--- src/components/tabbar/member.vue
+++ src/components/tabbar/member.vue
@@ -1,7 +1,43 @@
 <template>
     <div>
-       <h1>这是member组件</h1> 
-    </div>
+       <ul class="mui-table-view mui-table-view-chevron">
+			<li class="mui-table-view-cell mui-media">
+			  <a class="mui-navigate-right" href="#account">
+				<img class="mui-media-object mui-pull-left head-img" id="head-img" src="../../img/logo.png">
+				<div class="mui-media-body">
+					Hello LIUKE
+				    <p class='mui-ellipsis'>账号:hello liuke</p>
+				</div>
+			  </a>
+			</li>
+		</ul>
+        <ul class="mui-table-view mui-table-view-chevron">
+			<li class="mui-table-view-cell">
+			    <a href="#account" class="mui-navigate-right">账号与安全</a>
+			</li>
+		</ul>
+		<ul class="mui-table-view mui-table-view-chevron">
+			<li class="mui-table-view-cell">
+				<a href="#notifications" class="mui-navigate-right">新消息通知</a>
+			</li>
+			<li class="mui-table-view-cell">
+				<a href="#privacy" class="mui-navigate-right">隐私</a>
+			</li>
+		    <li class="mui-table-view-cell">
+				<a href="#general" class="mui-navigate-right">通用</a>
+			</li>
+		</ul>
+		<ul class="mui-table-view mui-table-view-chevron">
+	        <li class="mui-table-view-cell">
+		        <a href="#about" class="mui-navigate-right">关于MUI <i class="mui-pull-right update">V3.1.0</i></a>
+		    </li>
+	    </ul>
+		<ul class="mui-table-view">
+		    <li class="mui-table-view-cell" style="text-align: center;">
+				<a>退出登录</a>
+			</li>
+		</ul> 
+ </div>
 </template>
 
 <script>
